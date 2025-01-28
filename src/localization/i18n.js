28082a587a0df33b9a5edda06eaba7205761d84b
zeinammonem <zeinammonem@gmail.com>
2025-01-28 16:57:59 +0200

    smol changes

--- src/localization/i18n.js
+++ src/localization/i18n.js
@@ -2,15 +2,20 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
+const baseUrl = import.meta.env.BASE_URL;
+
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
+    backend: {
+      loadPath: `${baseUrl}locales/{{lng}}/{{ns}}.json`,
+    },
     fallbackLng: "en",
     debug: false,
-
     interpolation: {
       escapeValue: false,
     },
   });
+
 export default i18n;
